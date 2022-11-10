915da3b8479c9856111e95926b25cfc6f72fdf13
Rob Agar <rob@engineeredarts.co.uk>
2022-11-10 10:49:28 +0000

    async function, await in python

--- py-tritium-remote/python/test_py_tritium_remote/test.py
+++ py-tritium-remote/python/test_py_tritium_remote/test.py
@@ -1,3 +1,13 @@
+import asyncio
 import py_tritium_remote
 
-print(py_tritium_remote.sum_as_string(1, 1))
+print("sum_as_string", py_tritium_remote.sum_as_string(1, 1))
+
+
+async def main():
+    print("awaiting call_rust_sleep...")
+    await py_tritium_remote.call_rust_sleep()
+    print("...done")
+
+
+asyncio.run(main())
--- py-tritium-remote/src/lib.rs
+++ py-tritium-remote/src/lib.rs
@@ -1,4 +1,19 @@
 use pyo3::prelude::*;
+use std::time::Duration;
+
+/// Sleep for 1 second
+async fn rust_sleep() {
+    async_std::task::sleep(Duration::from_secs(1)).await;
+}
+
+#[pyfunction]
+fn call_rust_sleep(py: Python) -> PyResult<&PyAny> {
+    pyo3_asyncio::async_std::future_into_py(py, async move {
+        rust_sleep().await;
+        Ok(())
+    })
+}
+
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -9,6 +24,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn py_tritium_remote(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(call_rust_sleep, m)?)?;
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
