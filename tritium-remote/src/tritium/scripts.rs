2927b328a7a0068b496c88fd9e851488d407b3c3
Rob Agar <rob@engineeredarts.co.uk>
2022-11-21 16:53:42 +0000

    stop script for real

--- tritium-remote/src/tritium/scripts.rs
+++ tritium-remote/src/tritium/scripts.rs
@@ -35,4 +35,32 @@ impl Tritium {
             )),
         }
     }
+
+    pub async fn stop_script(
+        &mut self,
+        script_path: &str,
+    ) -> Result<ManuallyTriggerScriptManuallyTriggerScriptScript, TritiumError> {
+        let input = manually_trigger_script::ScriptTriggerInput {
+            action: manually_trigger_script::ScriptTriggerAction::STOP,
+            path: script_path.to_string(),
+        };
+        let operation =
+            QueryOperation::<ManuallyTriggerScript>::new(manually_trigger_script::Variables {
+                input,
+            });
+        let query = self.client.graphql_query(operation).await?;
+        let response = query.result.await?;
+
+        match response.data {
+            Some(data) => match data.manually_trigger_script.script {
+                Some(script) => Ok(script),
+                _ => Err(TritiumError::GenericError(
+                    "GraphQL response contained no script object".to_string(),
+                )),
+            },
+            _ => Err(TritiumError::GenericError(
+                "GraphQL response contained no data".to_string(),
+            )),
+        }
+    }
 }
