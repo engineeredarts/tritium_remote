ef3e47f6f5c2bd191fe32e25955c5af1984d5d3b
Rob Agar <rob@engineeredarts.co.uk>
2022-11-22 15:47:39 +0000

    1.0.0

--- py-tritium-remote/build_and_install.sh
+++ py-tritium-remote/build_and_install.sh
@@ -4,5 +4,5 @@ set -e
 
 maturin build
 
-pip3 install --force-reinstall ../target/wheels/py_tritium_remote-0.1.0-cp310-cp310-linux_x86_64.whl
+pip3 install --force-reinstall ../target/wheels/py_tritium_remote-1.0.0-cp310-cp310-linux_x86_64.whl
 
