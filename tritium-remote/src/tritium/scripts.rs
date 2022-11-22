d5ec827e337d2a83d0ef2e38d1dff8ce3f798319
Rob Agar <rob@engineeredarts.co.uk>
2022-11-22 10:12:52 +0000

    function return types

--- tritium-remote/src/tritium/scripts.rs
+++ tritium-remote/src/tritium/scripts.rs
@@ -8,10 +8,7 @@ use crate::graphql::QueryOperation;
 use crate::tritium::Tritium;
 
 impl Tritium {
-    pub async fn start_script(
-        &mut self,
-        script_path: &str,
-    ) -> Result<ManuallyTriggerScriptManuallyTriggerScriptScript, TritiumError> {
+    pub async fn start_script(&mut self, script_path: &str) -> Result<Script, TritiumError> {
         let input = manually_trigger_script::ScriptTriggerInput {
             action: manually_trigger_script::ScriptTriggerAction::START,
             path: script_path.to_string(),
@@ -25,7 +22,7 @@ impl Tritium {
 
         match response.data {
             Some(data) => match data.manually_trigger_script.script {
-                Some(script) => Ok(script),
+                Some(script) => Ok(Script::from(script)),
                 _ => Err(TritiumError::GenericError(
                     "GraphQL response contained no script object".to_string(),
                 )),
@@ -36,10 +33,7 @@ impl Tritium {
         }
     }
 
-    pub async fn stop_script(
-        &mut self,
-        script_path: &str,
-    ) -> Result<ManuallyTriggerScriptManuallyTriggerScriptScript, TritiumError> {
+    pub async fn stop_script(&mut self, script_path: &str) -> Result<Script, TritiumError> {
         let input = manually_trigger_script::ScriptTriggerInput {
             action: manually_trigger_script::ScriptTriggerAction::STOP,
             path: script_path.to_string(),
@@ -53,7 +47,7 @@ impl Tritium {
 
         match response.data {
             Some(data) => match data.manually_trigger_script.script {
-                Some(script) => Ok(script),
+                Some(script) => Ok(Script::from(script)),
                 _ => Err(TritiumError::GenericError(
                     "GraphQL response contained no script object".to_string(),
                 )),
@@ -61,6 +55,37 @@ impl Tritium {
             _ => Err(TritiumError::GenericError(
                 "GraphQL response contained no data".to_string(),
             )),
+        }
+    }
+}
+
+pub struct Script {
+    // path: Option<String>,
+    #[allow(dead_code)]
+    pub status: Option<ScriptStatus>,
+}
+
+pub enum ScriptStatus {
+    LOADED,
+    RUNNING,
+    ERROR,
+}
+
+impl From<manually_trigger_script::ScriptStatus> for ScriptStatus {
+    fn from(status: manually_trigger_script::ScriptStatus) -> ScriptStatus {
+        match status {
+            manually_trigger_script::ScriptStatus::LOADED => ScriptStatus::LOADED,
+            manually_trigger_script::ScriptStatus::RUNNING => ScriptStatus::RUNNING,
+            _ => ScriptStatus::ERROR,
+        }
+    }
+}
+
+impl From<ManuallyTriggerScriptManuallyTriggerScriptScript> for Script {
+    fn from(script: ManuallyTriggerScriptManuallyTriggerScriptScript) -> Script {
+        Script {
+            // path: script.path,
+            status: script.status.map(ScriptStatus::from),
         }
     }
 }
--- tritium-remote/src/tritium/system.rs
+++ tritium-remote/src/tritium/system.rs
@@ -1,21 +1,35 @@
 use crate::error::TritiumError;
-use crate::graphql::queries::basic_system_info::{
-    basic_system_info, basic_system_info::BasicSystemInfoSystem, BasicSystemInfo,
-};
+use crate::graphql::queries::basic_system_info::{basic_system_info, BasicSystemInfo};
 use crate::graphql::QueryOperation;
 use crate::tritium::Tritium;
 
 impl Tritium {
-    pub async fn query_basic_system_info(&mut self) -> Result<BasicSystemInfoSystem, TritiumError> {
+    pub async fn query_basic_system_info(&mut self) -> Result<SystemInfo, TritiumError> {
         let operation = QueryOperation::<BasicSystemInfo>::new(basic_system_info::Variables {});
         let query = self.client.graphql_query(operation).await?;
         let response = query.result.await?;
 
         match response.data {
-            Some(data) => Ok(data.system),
+            Some(data) => Ok(SystemInfo::from(data.system)),
             _ => Err(TritiumError::GenericError(
                 "GraphQL response contained no data".to_string(),
             )),
+        }
+    }
+}
+
+pub struct SystemInfo {
+    pub serial: String,
+    pub version: String,
+    pub name: Option<String>,
+}
+
+impl From<basic_system_info::BasicSystemInfoSystem> for SystemInfo {
+    fn from(info: basic_system_info::BasicSystemInfoSystem) -> SystemInfo {
+        SystemInfo {
+            serial: info.serial,
+            version: info.version,
+            name: info.name,
         }
     }
 }
