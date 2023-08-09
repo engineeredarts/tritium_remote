d75807f4c10afb9643b8bb359211a393574a52e2
Rob Agar <rob@engineeredarts.co.uk>
2023-08-09 11:26:33 +0100

    post message to scripts

---
+++ tritium-remote/examples/post_message_to_scripts.rs
@@ -0,0 +1,37 @@
+use serde_json::json;
+// use simple_logger::SimpleLogger;
+use std::env;
+use std::time::SystemTime;
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() {
+    // output *all* log messages, including from underlying transport
+    // SimpleLogger::new().env().init().unwrap();
+
+    let auth_token =
+        env::var("TRITIUM_AUTH_TOKEN").expect("TRITIUM_AUTH_TOKEN environment variable not set");
+
+    let mut tritium = tritium_remote::connect("ws://localhost:1234", &auth_token)
+        .await
+        .expect("failed to connect");
+
+    loop {
+        let t = seconds_since_unix_epoch();
+        let message = format!("The remote time is now {t}s since the start of 1970");
+        println!("posting to channel \"talking_clock\": {message}");
+        tritium
+            .post_message("talking_clock", json!(message))
+            .await
+            .expect("post failed");
+
+        sleep(Duration::from_secs(1)).await;
+    }
+}
+
+fn seconds_since_unix_epoch() -> u64 {
+    SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_secs()
+}
